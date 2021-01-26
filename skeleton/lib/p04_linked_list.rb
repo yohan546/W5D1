dcbe18159705a1cfb9bc3618f0c23544cc81510f
yohan oh <yohan.oh332@gmail.com>
2021-01-25 22:19:41 -0500

    linkedlist diagram corrected

--- skeleton/lib/p04_linked_list.rb
+++ skeleton/lib/p04_linked_list.rb
@@ -57,134 +57,164 @@ class LinkedList
   def append(key, val)
     new_node = Node.new(key, val)
 
-    # when we initalize, this is what our base case   
+# when we initalize, this is what our base case   
     # @head,@tail = sentinal_node
     # @head.next = @tail, @tail.prev = @head, so at start, it actually looks likes this      
 
     #             @head = sentinal_node                                                      @tail = sentintal_node  
     #                         |                                                                           | 
-    #                prev-nil, [next-sentinal_node]                                   [prev-sentinal_node], next-nil
-    #                                   |                                                       |
-    #                          prev-nil, next-nil                                    prev-nil, next-nil       
+    #                prev-nil, [next-@tail]                                                    [prev-@head], next-nil
+    #                                   |                                                           |
+    #                          prev-nil, next-nil                                           prev-nil, next-nil       
     
     
     #                                   now we append our first node node_1
-
+    # self.tail.prev.next = new_node
+    # new_node.prev = self.tail.prev
+    # new_node.next = self.tail
+    # self.tail.prev = new_node
     
-    # self.tail.prev.next = new_node
 
-    #                                                                                        
-    #                       @head                                                                       @tail  
+    #                          @head                                                                     @tail  
+    #                           |                                                                           | 
+    #                prev-nil, next-@tail                                                     prev-@head, next-nil
+    #                                   |                                                           |
+    #                          prev-nil, next-nil                                           prev-nil, [next-node_1]
+
+
+
+    #                        @head                                                                       @tail  
+    #                          |                                                                           | 
+    #                prev-nil, next-@tail                                                     prev-@head, next-nil
+    #                               |                                                               |
+    #                          prev-nil, next-nil                                           prev-nil, next-node_1
+    #                                                                                                           |
+    #                                                                                                       [prev-@head], [next-@tail]
+
+
+    #                      @head                                                                         @tail   
     #                         |                                                                           | 
-    #                 prev-nil, next-sentinal_node                                      prev-sentinal_node, next,nil
-    #                                                                                           |
-    #                                                                                 prev-nil, [next-node_1]  
+    #                prev-nil, next-@tail                                                     [prev-node_1], next-nil
+    #                                   |                                                           |
+    #                          prev-nil, next-nil                                           prev-@head, next-@tail
+
+   
+    #   at the end of our append method, initalize will call this, @head.next = @tail, @tail.prev = @head
 
 
+    #                       @head                                                                    @tail  
+    #                         |                                                                        | 
+    #                prev-nil, [next-@tail]                                                     [prev-@head], next-nil
+    #                                   |                                                           |
+    #                          prev-node_1, next-nil                                       perv-nil, next-@tail
+    #                                |                                                                  
+    #                       prev-@head, next-@tail
+    
+        
+    
+    #                                        now we append our second node node_2
+    # self.tail.prev.next = new_node
     # new_node.prev = self.tail.prev
     # new_node.next = self.tail
-
-    #                       @head                                                                       @tail   
-    #                         |                                                                           | 
-    #                 prev-nil, next-sentinal_node                                      prev-sentinal_node, next,nil
-    #                                                                                           |
-    #                                                                                 prev-nil, next-node_1
-    #                                                                                                 |
-    #                                                                             [prev-sentinal_node], [next-@tail]
-    #                                                                                                   
-
-
-    # self.tail.prev = new_node    
-                                                                                            
-    #                       @head                                                                       @tail   
-    #                         |                                                                           | 
-    #                 prev-nil, next-sentinal_node                                      [prev-node_1], next,nil
-    #                                                                                         |
-    #                                                                      prev-sentinal_node, next-@tail    
-
-
-    #  Remember at initalize, @head.next = @tail, @tail.prev = @head, so this next step actually occurs at every iteration
-
-    #                       @head                                                                       @tail 
-    #                         |                                                                           | 
-    #                 prev-nil, next-sentinal_node                                      prev-node_1, next,nil
-    #                                                                                         |
-    #                                                                    [prev-sentinal_node], [next-@tail]    
-
-
-    #                                                 Now we append node_2 
-
-
-    # self.tail.prev.next = new_node
-
-    #                       @head                                                                       @tail 
-    #                         |                                                                           | 
-    #                 prev-nil, next-sentinal_node                                      prev-node_1, next,nil
-    #                                                                                         |
-    #                                                                    prev-sentinal_node, [next-node_2]   
-
-
-    # new_node.prev = self.tail.prev
-    # new_node.next = self.tail
-
-    #                       @head                                                                       @tail 
-    #                         |                                                                           | 
-    #                 prev-nil, next-sentinal_node                                      prev-node_1, next,nil
-    #                                                                                         |
-    #                                                                    prev-sentinal_node, next-node_2   
-    #                                                                                             |
-    #                                                                                         [prev-node_1],[next-@tail]
-
-
     # self.tail.prev = new_node
 
-    #                       @head                                                                       @tail 
-    #                         |                                                                           | 
-    #                 prev-nil, next-sentinal_node                                      [prev-node_2], next,nil
-    #                                                                                         |
-    #                                                                             prev-node_1, next-@tail
+
+    #                      @head                                                                    @tail  
+    #                         |                                                                        | 
+    #                prev-nil, next-@tail                                                     prev-@head, next-nil
+    #                                   |                                                           |
+    #                          prev-node_1, next-nil                                       perv-nil, [next-node_2]
+    #                                |                                                                  
+    #                       prev-@head, next-@tail   
+    
+
+    #                      @head                                                                    @tail  
+    #                         |                                                                        | 
+    #                prev-nil, next-@tail                                                     prev-@head, next-nil
+    #                                   |                                                           |
+    #                          prev-node_1, next-nil                                       perv-nil, next-node_2
+    #                                |                                                                     |     
+    #                       prev-@head, next-@tail                                                     [prev-@head], [next-@tail]           
+    
+    
+    #                      @head                                                                    @tail  
+    #                         |                                                                        | 
+    #                prev-nil, [next-@tail]                                                     [prev-node_2], next-nil
+    #                                   |                                                           |
+    #                          prev-node_1, next-nil                                        prev-@head, next-@tail
+    #                                |                                                                         
+    #                       prev-@head, next-@tail                
+
+        
+    #                      @head                                                                    @tail  
+    #                         |                                                                        | 
+    #                prev-nil, [next-@tail]                                                     [prev-node_2], next-nil
+    #                                   |                                                           |
+    #                          prev-node_1, next-nil                                        prev-@head, next-@tail
+    #                                |                                                                         
+    #                       prev-@head, next-@tail    
+
+    #   at the end of our append method, initalize will call this again , @head.next = @tail, @tail.prev = @head
+
+    #                      @head                                                                    @tail  
+    #                         |                                                                        | 
+    #                prev-nil, [next-@tail]                                                     [prev-@head], next-nil
+    #                                   |                                                           |
+    #                          prev-node_2, next-nil                                        prev-nil, next-@tail
+    #                                |                                                                    |     
+    #                       prev-@head, next-@tail                                             prev-node_1, next-nil
+    #                                                                                                |
+    #                                                                                           prev-@head, next @tail
 
 
-    #                                                 now we append node_3
-
-
+    #                                           now we append our third node node_3
     # self.tail.prev.next = new_node
-
-    #                       @head                                                                       @tail 
-    #                         |                                                                           | 
-    #                 prev-nil, next-sentinal_node                                      prev-node_2, next,nil
-    #                                                                                         |
-    #                                                                             prev-node_1, [next-node_3]
-    
-    
     # new_node.prev = self.tail.prev
     # new_node.next = self.tail
-    
-    #                       @head                                                                       @tail 
-    #                         |                                                                           | 
-    #                 prev-nil, next-sentinal_node                                      prev-node_2, next,nil
-    #                                                                                         |
-    #                                                                             prev-node_1, next-node_3
-    #                                                                                               |
-    #                                                                                  [prev-node_2],[next-@tail]
-
-
     # self.tail.prev = new_node
 
-    #                       @head                                                                       @tail 
-    #                         |                                                                           | 
-    #                 prev-nil, next-sentinal_node                                      [prev-node_3], next,nil
-    #                                                                                          |
-    #                                                                               prev-node_2, next-@tail
 
 
-    #                       @head                                                                       @tail 
-    #                         |                                                                           | 
-    #                 prev-nil, next-@tail                                                      @head, next,nil
-    #                                   |                                                          |
-    #                       [prev-node_3], next,nil                                       prev-nil, next-@head
-    #                              |     
-    #                  |prev-node_2, next-@tail
+    #                      @head                                                                    @tail  
+    #                         |                                                                        | 
+    #                prev-nil, next-@tail                                                     prev-@head, next-nil
+    #                                   |                                                           |
+    #                          prev-node_2, next-nil                                        prev-nil, [next-node_3]
+    #                                |                                                                         
+    #                       prev-@head, next-@tail                                             
+                                                                                                    
+    
+    
+    #                      @head                                                                    @tail  
+    #                         |                                                                        | 
+    #                prev-nil, next-@tail                                                     prev-@head, next-nil
+    #                                   |                                                           |
+    #                          prev-node_2, next-nil                                        prev-nil, next-node_3
+    #                                |                                                                      |         
+    #                       prev-@head, next-@tail                                                      [prev-@head], [next-@tail] 
+
+
+    #                      @head                                                                    @tail  
+    #                         |                                                                        | 
+    #                prev-nil, next-@tail                                                     [prev-node_3], next-nil
+    #                              |                                                               |
+    #                          prev-node_2, next-nil                                        prev-@head, next-@tail 
+    #                                |                                                                              
+    #                       prev-@head, next-@tail                 
+
+
+    #   at the end of our append method, initalize will call this again , @head.next = @tail, @tail.prev = @head
+
+
+    #                      @head                                                                    @tail  
+    #                         |                                                                        | 
+    #                prev-nil, next-@tail                                                     [prev-@head], next-nil
+    #                              |                                                               |
+    #                          prev-node_3, next-nil                                        prev-nil, next-@tail 
+    #                                |                                                                       |       
+    #                       prev-@head, next-@tail                                                      prev-node_2, next-nil 
+    #                                                                                                       |
+    #                                                                                                   prev-@head, next-@tail
 
 
 
