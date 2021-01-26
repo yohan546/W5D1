c6669e6ce884572d002a3487e50f265d6b0aeb84
yohan oh <yohan.oh332@gmail.com>
2021-01-25 21:00:51 -0500

    updated linked_list diagram

--- skeleton/lib/p04_linked_list.rb
+++ skeleton/lib/p04_linked_list.rb
@@ -57,49 +57,137 @@ class LinkedList
   def append(key, val)
     new_node = Node.new(key, val)
 
-    #                   head_node -> node_1                       node_2 <- tail_node
-    #                                  |                             |
-    #                              prev-nil, next-nil       prev-nil, next-node_3
-    #                                                           
-    # append node_3
+    # when we initalize, this is what our base case   
+    # @head,@tail = sentinal_node
+    # @head.next = @tail, @tail.prev = @head, so at start, it actually looks likes this      
 
-    #                   head_node -> node_1                       node_2 <- tail_node
-    #                                  |                             |
-    #                              prev-nil, next-nil       prev-nil, next-node3
-    #                                                                         |
-    #                                                              prev-node_2, next-tail_node
-
-
-    #                  head_node -> node_1                      next_node3 <- tail_node
-    #                                 |                             |
-    #                             prev-nil, next-nil    prev-node_2, next-tail_node
-    #                                                                                 
-    # 
-     #                  head_node -> node_1                      next_node3 <- tail_node
-    #                                 |                             |
-    #                             prev-nil, next-nil    prev-node_2, next-tail_node    
+    #             @head = sentinal_node                                                      @tail = sentintal_node  
+    #                         |                                                                           | 
+    #                prev-nil, [next-sentinal_node]                                   [prev-sentinal_node], next-nil
+    #                                   |                                                       |
+    #                          prev-nil, next-nil                                    prev-nil, next-nil       
     
-    #append node_4
-   #                  head_node -> node_1                      next_node3 <- tail_node
-    #                                 |                             |
-    #                             prev-nil, next-nil    prev-node_2, next-node_4    
+    
+    #                                   now we append our first node node_1
+
+    
+    # self.tail.prev.next = new_node
+
+    #                                                                                        
+    #                       @head                                                                       @tail  
+    #                         |                                                                           | 
+    #                 prev-nil, next-sentinal_node                                      prev-sentinal_node, next,nil
+    #                                                                                           |
+    #                                                                                 prev-nil, [next-node_1]  
+
+
+    # new_node.prev = self.tail.prev
+    # new_node.next = self.tail
+
+    #                       @head                                                                       @tail   
+    #                         |                                                                           | 
+    #                 prev-nil, next-sentinal_node                                      prev-sentinal_node, next,nil
+    #                                                                                           |
+    #                                                                                 prev-nil, next-node_1
+    #                                                                                                 |
+    #                                                                             [prev-sentinal_node], [next-@tail]
+    #                                                                                                   
+
+
+    # self.tail.prev = new_node    
+                                                                                            
+    #                       @head                                                                       @tail   
+    #                         |                                                                           | 
+    #                 prev-nil, next-sentinal_node                                      [prev-node_1], next,nil
+    #                                                                                         |
+    #                                                                      prev-sentinal_node, next-@tail    
+
+
+    #  Remember at initalize, @head.next = @tail, @tail.prev = @head, so this next step actually occurs at every iteration
+
+    #                       @head                                                                       @tail 
+    #                         |                                                                           | 
+    #                 prev-nil, next-sentinal_node                                      prev-node_1, next,nil
+    #                                                                                         |
+    #                                                                    [prev-sentinal_node], [next-@tail]    
+
+
+    #                                                 Now we append node_2 
+
+
+    # self.tail.prev.next = new_node
+
+    #                       @head                                                                       @tail 
+    #                         |                                                                           | 
+    #                 prev-nil, next-sentinal_node                                      prev-node_1, next,nil
+    #                                                                                         |
+    #                                                                    prev-sentinal_node, [next-node_2]   
+
+
+    # new_node.prev = self.tail.prev
+    # new_node.next = self.tail
+
+    #                       @head                                                                       @tail 
+    #                         |                                                                           | 
+    #                 prev-nil, next-sentinal_node                                      prev-node_1, next,nil
+    #                                                                                         |
+    #                                                                    prev-sentinal_node, next-node_2   
+    #                                                                                             |
+    #                                                                                         [prev-node_1],[next-@tail]
+
+
+    # self.tail.prev = new_node
+
+    #                       @head                                                                       @tail 
+    #                         |                                                                           | 
+    #                 prev-nil, next-sentinal_node                                      [prev-node_2], next,nil
+    #                                                                                         |
+    #                                                                             prev-node_1, next-@tail
+
+
+    #                                                 now we append node_3
+
+
+    # self.tail.prev.next = new_node
+
+    #                       @head                                                                       @tail 
+    #                         |                                                                           | 
+    #                 prev-nil, next-sentinal_node                                      prev-node_2, next,nil
+    #                                                                                         |
+    #                                                                             prev-node_1, [next-node_3]
+    
+    
+    # new_node.prev = self.tail.prev
+    # new_node.next = self.tail
+    
+    #                       @head                                                                       @tail 
+    #                         |                                                                           | 
+    #                 prev-nil, next-sentinal_node                                      prev-node_2, next,nil
+    #                                                                                         |
+    #                                                                             prev-node_1, next-node_3
+    #                                                                                               |
+    #                                                                                  [prev-node_2],[next-@tail]
+
+
+    # self.tail.prev = new_node
+
+    #                       @head                                                                       @tail 
+    #                         |                                                                           | 
+    #                 prev-nil, next-sentinal_node                                      [prev-node_3], next,nil
+    #                                                                                          |
+    #                                                                               prev-node_2, next-@tail
+
+
+    #                       @head                                                                       @tail 
+    #                         |                                                                           | 
+    #                 prev-nil, next-@tail                                                      @head, next,nil
+    #                                   |                                                          |
+    #                       [prev-node_3], next,nil                                       prev-nil, next-@head
+    #                              |     
+    #                  |prev-node_2, next-@tail
 
 
 
-
-      #                  head_node -> node_1                      next_node3 <- tail_node
-    #                                 |                             |
-    #                             prev-nil, next-nil    prev-node_2, next-node_4    
-        #                                                                  |
-        #                                                         prev-node_2, next-tail_node
-
-
-
-      #                  head_node -> node_1                      next_node4 <- tail_node
-    #                                 |                             |
-    #                             prev-nil, next-nil    prev-node_2, next-node_3    
-        #                                                                  |
-        #                                                         prev-node_2, next-tail_node
     
     self.tail.prev.next = new_node
     new_node.prev = self.tail.prev
